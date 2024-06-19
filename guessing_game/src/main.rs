46eefb8a5d3cf7bd4d6850ab041e7a46b501492d
vo39 <vo39770@gmail.com>
2024-06-20 03:15:26 +0900

    practice guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,17 +1,41 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    // 変数のはデフォルトで不変 (immutable)
-    // mut修飾子をつけると変数が可変になる (mutable)
-    let mut guess = String::new();
+    println!("The secret number is: {}", secret_number);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        // 変数のはデフォルトで不変 (immutable)
+        // mut修飾子をつけると変数が可変になる (mutable)
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // 変数を新しい値で宣言することが許されている (シャドーイング)
+        // 重複しない変数を2つ作る代わりに変数名の再利用ができる
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
